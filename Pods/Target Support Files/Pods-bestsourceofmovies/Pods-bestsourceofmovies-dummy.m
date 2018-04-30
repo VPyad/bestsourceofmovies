a95efe746fb25d5213d14ac9e299c148019424a8
Vadim <test@test.test>
2018-04-30 12:16:34 +0300

    add pods

---
+++ Pods/Target Support Files/Pods-bestsourceofmovies/Pods-bestsourceofmovies-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bestsourceofmovies : NSObject
+@end
+@implementation PodsDummy_Pods_bestsourceofmovies
+@end
---
+++ Pods/Target Support Files/Pods-bestsourceofmoviesTests/Pods-bestsourceofmoviesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bestsourceofmoviesTests : NSObject
+@end
+@implementation PodsDummy_Pods_bestsourceofmoviesTests
+@end
